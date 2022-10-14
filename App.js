8d98d43467357b101cf9dc528ae619dc9907b42c
yanness168 <94063697+yanness168@users.noreply.github.com>
2022-10-14 15:42:59 -0400

    push

--- App.js
+++ App.js
@@ -1,12 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
+import * as React from 'react';
+import { useState,useEffect } from 'react';
+import { Text, View, Button, StyleSheet, FlatList } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 
-import { StyleSheet, Text, View } from 'react-native';
+const API = "https://jsonplaceholder.typicode.com/todos";
+
+/* Desctructure the "item" */
+const renderToDo = ({item}) => <Todo id={item.id} userId={item.userId} t={item.title} c={item.completed.toString()}/>;
+
+/* The templete of a list */
+const Todo = (props) =>(
+  <View style={styles.item}>
+    <Text style={styles.text}>Id: {props.id}</Text>
+    <Text style={styles.text}>User Id: {props.userId}</Text>
+    <Text style={styles.text}>To-do title: {props.t}</Text>
+    <Text style={styles.status}>Status: {props.c}</Text>
+  </View>
+)
 
 export default function App() {
+
+  const [data,setData] = useState([]);
+  const [status,setStatus] = useState(" ");
+  const [filteredData, setFilteredData] = useState(data);
+
+  useEffect(()=>{
+    fetch(API)
+      .then((res)=>{
+        return res.json()
+      })
+      .then((json)=>{
+        setData(json);
+        setFilteredData(json);
+      })
+      .catch((err)=>{
+        console.log(err.message)
+      });
+  },[]);
+
+  const filter = (value) =>{
+    setStatus(value);
+    if(value ==="Yes"){
+      setFilteredData(data.filter(item => item.completed.toString()=="true"));
+    }else{
+      setFilteredData(data.filter(item => item.completed.toString()=="false"));
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+    <Text>Your completion status is: {status}</Text>
+    <Picker selectedValue={status} onValueChange={filter}>
+      <Picker.Item label="Yes" value="Yes"/>
+      <Picker.Item label="No" value="No"/>
+    </Picker>
+
+    <FlatList style={styles.todoList}
+      data={filteredData}
+      renderItem={renderToDo}
+      keyExtractor={item=>item.id}>
+      </FlatList>
     </View>
   );
 }
@@ -18,4 +71,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  item:{
+    borderWidth:1,
+    padding:10,
+    margin: 5,
+    borderRadius: 5,
+    backgroundColor: "pink",
+    margin: 10,
+  },
+  todoList:{
+    alignContent:'Stretch',
+    width:"100%",
+  },
+  text: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  status: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: "yellow",
+    }
 });
